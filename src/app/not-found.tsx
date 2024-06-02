796572c82d454202df9d3d2bac90860695e473cb
Pavel Salianik <solyqnik@gmail.com>
2024-06-02 19:16:58 +0300

    добавил адаптив для 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
-import MainLayout from "@/components/layouts/MainLayout";
-import React, { FC } from "react";
+import { FC } from "react";
 import Link from "next/link";
+import MainLayout from "@/components/layouts/MainLayout";
 import { Container } from "@/components/ui/Wrappers";
 
 const NotFound: FC = () => {
@@ -8,15 +8,15 @@ const NotFound: FC = () => {
     <MainLayout>
       <Container className="flex justify-center items-center m-auto">
         <div className="flex flex-col items-center justify-center bg-white p-5 sm:p-10 max-w-2xl rounded-lg shadow-sm">
-          <div className="font-semibold text-4xl text-center mb-8">
+          <div className="font-semibold text-2xl sm:text-4xl text-center mb-4 sm:mb-8">
             Страница не найдена
           </div>
-          <div className=" text-center text-gray-600 mb-8 text-xl">
+          <div className=" text-center text-gray-600 mb-4 sm:mb-8 text-lg sm:text-xl">
             Такой страницы не существует либо она была перенесана на другой
             адрес
           </div>
           <Link
-            className=" bg-cyan-800 lg:hover:bg-cyal-900 text-white font-medium py-2 px-6 rounded text-xl"
+            className=" bg-cyan-800 lg:hover:bg-cyal-900 text-white font-medium py-2 px-6 rounded sm:text-base text-xl"
             href="/"
           >
             На главную
