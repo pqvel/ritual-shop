b04f807df37a82555067ee58364eb8b47d057469
Pavel Salianik <solyqnik@gmail.com>
2024-06-16 12:39:12 +0300

    +

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,8 @@
-"use client";
 import { FC } from "react";
 import LoginForm from "./form";
-import { showPassword } from "../actions/uslugi";
 
 const LoginPage: FC = () => {
-  return (
-    <div>
-      <form action={showPassword}>
-        <button type="submit">ddwd</button>
-      </form>
-      <LoginForm />;
-    </div>
-  );
+  return <LoginForm />;
 };
 
 export default LoginPage;
